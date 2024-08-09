0f0a0ae7616c21eaef4382b96cbfc27a221ddb7e
DuoLi1999 <liduo1202@gmail.com>
2024-08-09 23:07:21 +0800

    update

--- language_understanding/deberta/bash.sh
+++
@@ -1,5 +0,0 @@
-sleep 4h
-python classify.py --idx 0 --t 30 --a 0.5
-python classify.py --idx 2 --t 30 --a 1
-python classify.py --idx 5 --t 30 --a 1
-python classify.py --idx 7 --t 30 --a 1
---
+++ language_understanding/deberta/rub_deberta.sh
@@ -0,0 +1,2 @@
+# idx represents the idx of glue, p is the dropout ratio, and a is short of the alpha
+python classify.py --idx 0 --p 0.1 --a 0.1
